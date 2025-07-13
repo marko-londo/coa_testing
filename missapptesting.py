17892b6d5fac224fd734862744e647b3b5a99414
marko-londo <48113935+marko-londo@users.noreply.github.com>
2025-07-12 23:04:36 -0400

    Update missapptesting.py

--- missapptesting.py
+++ missapptesting.py
@@ -11,7 +11,7 @@ import re
 import dropbox
 
 st.set_page_config(
-    page_title="Missed Stops Manager | JP Mascaro & Sons",
+    page_title="CoA Misses | JP Mascaro & Sons",
     page_icon="https://raw.githubusercontent.com/marko-londo/coa_testing/refs/heads/main/favicon.ico",
     layout="centered",  # or "wide"
     initial_sidebar_state="expanded",
