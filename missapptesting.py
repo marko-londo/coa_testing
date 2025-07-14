257415968e89562a615b7db0c0f36869c5b88793
marko-londo <48113935+marko-londo@users.noreply.github.com>
2025-07-13 21:51:20 -0400

    Update missapptesting.py

--- missapptesting.py
+++ missapptesting.py
@@ -85,13 +85,12 @@ def updates():
     
     st.markdown(
         f"""
-        <div style='display: flex; justify-content: center; align-items: center; margin-bottom: 12px;'>
+        <div style='margin-bottom: 12px;'>
             <img src='{jpm_logo}' width='320'>
         </div>
         """,
         unsafe_allow_html=True
     )
-
 
     st.markdown("""
         <style>
